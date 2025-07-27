655e66ca1e14eeb4fcb676ec79de972782352a55
Sanjiv Thapa <thapasvt12@gmail.com>
2025-07-27 08:54:03 +0545

    Updated app to include router from auth and also implemented creatre_db_and_tables on start

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from core.database import create_db_and_tables
+from auth.routes import router as auth_router
 
-app = FastAPI()
+load_dotenv()
 
-app.get("/")
-def hello():
-    return {"Hello": "World"}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to FastAPI Auth System"}
