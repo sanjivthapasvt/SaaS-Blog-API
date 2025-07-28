3cfec799270a59f3f78cec079af6ef99a6859fc6
Sanjiv Thapa <thapasvt12@gmail.com>
2025-07-28 16:17:50 +0545

    Updated route to include users route

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from core.database import create_db_and_tables
 from auth.routes import router as auth_router
 from blogs.routes import router as blog_router
 from fastapi.staticfiles import StaticFiles
-
+from users.routes import router as users_router
 
 load_dotenv()
 
@@ -22,7 +22,8 @@ app = FastAPI(lifespan=lifespan)
 app.mount("/media", StaticFiles(directory="media"), name="media")
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(blog_router, prefix="/blog", tags=["Blog"])
+app.include_router(users_router, prefix="/user", tags=["Users"])
 
 @app.get("/")
 def home():
-    return {"message": "Welcome to FastAPI Auth System"}
+    return {"message": "Welcome to SaaS-blog-API"}
