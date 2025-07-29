d6a24cc8de6a4d6bebffbe974d2ea3f8d82ff9b6
Sanjiv Thapa <thapasvt12@gmail.com>
2025-07-29 10:01:46 +0545

    Added route for unfollow user, list follwer and following and updated model for timezone to be type timezone

--- app/users/models.py
+++ app/users/models.py
@@ -1,10 +1,12 @@
 from sqlmodel import SQLModel, Field, Relationship
-from datetime import datetime
+from datetime import timezone, datetime
 from typing import List
 
 class UserFollowLink(SQLModel, table=True):
     follower_id: int | None = Field(default=None,foreign_key="user.id" ,primary_key=True)
     following_id: int | None = Field(default=None,foreign_key="user.id", primary_key=True)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
 
 
 class User(SQLModel, table=True):
@@ -12,7 +14,7 @@ class User(SQLModel, table=True):
     username: str = Field(unique=True, index=True,)
     email: str = Field(unique=True, index=True)
     full_name: str = Field(default=None)
-    joined_at: str | None= Field(default=datetime.now())
+    joined_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     hashed_password: str
 
 
@@ -21,7 +23,8 @@ class User(SQLModel, table=True):
         link_model=UserFollowLink,
         sa_relationship_kwargs={
             "primaryjoin": "User.id==UserFollowLink.follower_id",
-            "secondaryjoin": "User.id==UserFollowLink.following_id" 
+            "secondaryjoin": "User.id==UserFollowLink.following_id",
+            "overlaps": "followers",
         }
     )
 
@@ -30,6 +33,7 @@ class User(SQLModel, table=True):
         link_model=UserFollowLink,
         sa_relationship_kwargs={
             "primaryjoin": "User.id==UserFollowLink.following_id",
-            "secondaryjoin": "User.id==UserFollowLink.follower_id"
+            "secondaryjoin": "User.id==UserFollowLink.follower_id",
+            "overlaps": "followings"
         }
     )
--- app/users/routes.py
+++ app/users/routes.py
@@ -6,7 +6,9 @@ from core.database import Session, get_session
 from auth.auth import get_current_user
 from sqlmodel import select
 from users.schema import UserRead
+
 router = APIRouter()
+
 
 @router.post("/follow/{user_id}")
 async def follow_user(
@@ -38,6 +40,36 @@ async def follow_user(
     return {"message": f"You are now following {target_user.username}"}
 
 
+
+@router.post("/unfollow/{user_id}")
+async def unfollow_user(
+    user_id: int,
+    session: Session = Depends(get_session),
+    current_user:User = Depends(get_current_user),
+):
+    target_user = session.get(User, user_id)
+
+    if not target_user:
+        raise HTTPException(status_code=404, detail="User doesnot exist")
+    if target_user.id == current_user.id:
+        raise HTTPException(status_code=400, detail="Cannot follow yourself")
+    
+    #Check if the user is following
+    link = session.exec(select(UserFollowLink).where(
+        UserFollowLink.follower_id == current_user.id,
+        UserFollowLink.following_id == target_user.id
+    )).first()
+
+    if not link:
+        raise HTTPException(status_code=400, detail="You have not followed the user")
+
+    session.delete(link)
+    session.commit()
+
+    return {"message": f"You have succesfully unfollowed {target_user.username}"}
+
+
+
 @router.get("/list", response_model=List[UserRead])
 async def list_all_users(
     session: Session = Depends(get_session),
@@ -46,3 +78,47 @@ async def list_all_users(
     users = session.exec(select(User.id, User.full_name).where(User.id != current_user.id)).all()
  
     return users
+
+
+
+@router.get("/list/followers", response_model=List[UserRead])
+async def list_followers(
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user)
+):
+    try:
+        raw_followers = session.exec(
+            select(UserFollowLink.follower_id).where(UserFollowLink.following_id == current_user.id)
+        ).all()
+
+        followers = session.exec(
+            select(User).where(User.id.in_(raw_followers)) # type: ignore
+        ).all()
+        
+        return followers
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Something went wrong while listing followers {str(e)}")
+
+
+
+@router.get("/list/followings", response_model=List[UserRead])
+async def list_followings(
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user)
+):
+    raw_followings = []
+    followings = []
+    try:  
+        raw_followings = session.exec(
+            select(UserFollowLink.following_id).where(UserFollowLink.follower_id == current_user.id)
+            ).all()
+        
+        if raw_followings:
+            followings = session.exec(
+                select(User).where(User.id.in_(raw_followings)) # type: ignore
+                ).all()
+            
+        return followings
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Something went wrong {str(e)}")
