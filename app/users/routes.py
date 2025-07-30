1b85b6fdc87005b948de87d17d5877a637bea0cf
Sanjiv Thapa <thapasvt12@gmail.com>
2025-07-30 12:58:38 +0545

    added unfollow user route and change password route

--- app/users/routes.py
+++ app/users/routes.py
@@ -6,6 +6,8 @@ from core.database import Session, get_session
 from auth.auth import get_current_user
 from sqlmodel import select
 from users.schema import UserRead
+from auth.utils import hash_password, verify_password
+
 
 router = APIRouter()
 
@@ -16,68 +18,118 @@ async def follow_user(
     session: Session = Depends(get_session),
     current_user:User = Depends(get_current_user),
 ):
-    target_user = session.get(User, user_id)
+    try:
+        target_user = session.get(User, user_id)
 
-    if not target_user:
-        raise HTTPException(status_code=404, detail="User doesnot exist")
-    if target_user.id == current_user.id:
-        raise HTTPException(status_code=400, detail="Cannot follow yourself")
+        if not target_user:
+            raise HTTPException(status_code=404, detail="User doesnot exist")
+        if target_user.id == current_user.id:
+            raise HTTPException(status_code=400, detail="Cannot follow yourself")
+        
+        #Check if the user is already following
+        link = session.exec(select(UserFollowLink).where(
+            UserFollowLink.follower_id == current_user.id,
+            UserFollowLink.following_id == target_user.id
+        )).first()
+
+        if link:
+            raise HTTPException(status_code=400, detail="Already following")
+        
+        newlink = UserFollowLink(follower_id=current_user.id, following_id=target_user.id)
+
+        session.add(newlink)
+        session.commit()
+
+        return {"message": f"You are now following {target_user.username}"}
     
-    #Check if the user is already following
-    link = session.exec(select(UserFollowLink).where(
-        UserFollowLink.follower_id == current_user.id,
-        UserFollowLink.following_id == target_user.id
-    )).first()
+    except HTTPException:
+        raise
 
-    if link:
-        raise HTTPException(status_code=400, detail="Already following")
-    
-    newlink = UserFollowLink(follower_id=current_user.id, following_id=target_user.id)
-
-    session.add(newlink)
-    session.commit()
-
-    return {"message": f"You are now following {target_user.username}"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"{str(e)}")
 
 
-
-@router.post("/unfollow/{user_id}")
+@router.delete("/unfollow/{user_id}")
 async def unfollow_user(
     user_id: int,
     session: Session = Depends(get_session),
     current_user:User = Depends(get_current_user),
 ):
-    target_user = session.get(User, user_id)
+    try:
+        target_user = session.get(User, user_id)
 
-    if not target_user:
-        raise HTTPException(status_code=404, detail="User doesnot exist")
-    if target_user.id == current_user.id:
-        raise HTTPException(status_code=400, detail="Cannot follow yourself")
+        if not target_user:
+            raise HTTPException(status_code=404, detail="User doesnot exist")
+        if target_user.id == current_user.id:
+            raise HTTPException(status_code=400, detail="Cannot follow yourself")
+        
+        #Check if the user is following
+        link = session.exec(select(UserFollowLink).where(
+            UserFollowLink.follower_id == current_user.id,
+            UserFollowLink.following_id == target_user.id
+        )).first()
+
+        if not link:
+            raise HTTPException(status_code=400, detail="You have not followed the user")
+
+        session.delete(link)
+        session.commit()
+
+        return {"message": f"You have succesfully unfollowed {target_user.username}"}
+
+    except HTTPException:
+        raise
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"{str(e)}")
+
+
+@router.patch("/changepassword")
+def change_password(
+    current_password: str,
+    new_password: str,
+    again_new_password: str,
+    session: Session =  Depends(get_session),
+    current_user: User = Depends(get_current_user)
+):
+    try:
+        if not verify_password(current_password, current_user.hashed_password):
+            raise HTTPException(status_code=400, detail="Your old password doesn't match")
+
+        if len(new_password) < 6:
+            raise HTTPException(status_code=400, detail="Password length must be greater than 6")
+
+        if new_password != again_new_password:
+            raise HTTPException(status_code=400, detail="New passwords doesn't match")
+        
+        hashed_new_password = hash_password(new_password)
+
+        current_user.hashed_password = hashed_new_password
+        session.add(current_user)
+        session.commit()
+        return {"detail": "Successfully changed password"}
     
-    #Check if the user is following
-    link = session.exec(select(UserFollowLink).where(
-        UserFollowLink.follower_id == current_user.id,
-        UserFollowLink.following_id == target_user.id
-    )).first()
+    except HTTPException:
+        raise
 
-    if not link:
-        raise HTTPException(status_code=400, detail="You have not followed the user")
-
-    session.delete(link)
-    session.commit()
-
-    return {"message": f"You have succesfully unfollowed {target_user.username}"}
-
-
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Something went wrong {str(e)}")
 
 @router.get("/list", response_model=List[UserRead])
 async def list_all_users(
     session: Session = Depends(get_session),
     current_user:User = Depends(get_current_user)
 ):
-    users = session.exec(select(User.id, User.full_name).where(User.id != current_user.id)).all()
+    try:
+        users = session.exec(select(User.id, User.full_name).where(User.id != current_user.id)).all()
  
-    return users
+        return users
+    
+    except HTTPException:
+        raise
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"{str(e)}")
 
 
 
@@ -96,6 +148,9 @@ async def list_followers(
         ).all()
         
         return followers
+    except HTTPException:
+        raise
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Something went wrong while listing followers {str(e)}")
 
@@ -119,6 +174,8 @@ async def list_followings(
                 ).all()
             
         return followings
-    
+    except HTTPException:
+        raise
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Something went wrong {str(e)}")
