d7228c1c11642bce5782bfd8e358802e95edce23
Sanjiv Thapa <thapasvt12@gmail.com>
2025-08-02 13:33:35 +0545

    Clener code

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -10,12 +10,12 @@ import os
 
 SECRET_KEY = os.getenv("SECRET_KEY") or "sanjivthapafastapisecretkey"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 bearer_scheme = HTTPBearer()
 
 
-def create_access_token(data: dict, expires_delta: timedelta = None) -> str: # type: ignore
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
--- app/auth/utils.py
+++ app/auth/utils.py
@@ -1,8 +1,4 @@
-
-from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
