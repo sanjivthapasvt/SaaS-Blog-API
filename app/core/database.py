60adbf551698f8b83fa73848f7d4d33cfd190eed
Sanjiv Thapa <thapasvt12@gmail.com>
2025-08-02 13:33:48 +0545

    Turned off echo=False for now

--- app/core/database.py
+++ app/core/database.py
@@ -3,7 +3,7 @@ from sqlmodel import SQLModel, create_engine, Session
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-engine = create_engine(sqlite_url, echo=True)
+engine = create_engine(sqlite_url, echo=False)
 connect_args = {"check_same_thread": False}
 
 
