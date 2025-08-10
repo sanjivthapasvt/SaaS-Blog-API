c1be424abe35672949b19b9d6718d3dbb7ff6ba1
Sanjiv Thapa <thapasvt12@gmail.com>
2025-08-10 16:11:18 +0545

    Changed from sync to async function

--- app/notifications/notification_service.py
+++ app/notifications/notification_service.py
@@ -1,18 +1,18 @@
-from sqlmodel import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from notifications.models import NotificationType, Notification
 from utils.logger import logger
 
-def create_notfication(
+async def create_notfication(
     user_id: int,
     message: str,
     notification_type: NotificationType,
-    session: Session,
+    session: AsyncSession,
 ):
     try:
         notification = Notification(user_id=user_id, notification_type=notification_type, message=message)
         session.add(notification)
-        session.commit()
-        session.refresh(notification)
+        await session.commit()
+        await session.refresh(notification)
     
     except Exception as e:
         logger.error(f"Something went wrong while creating notification {str(e)}")
