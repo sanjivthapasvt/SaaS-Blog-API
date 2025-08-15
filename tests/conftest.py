9fab3c9db5dcd63f60fa536ce570c90853e59850
Sanjiv Thapa <thapasvt12@gmail.com>
2025-08-15 20:51:38 +0545

    ✅ Updated confests for handeling lifespan better and also setting TESTING variable 1

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,26 @@
 import os
-import asyncio
-import pytest
 import pytest_asyncio
-
-os.environ["DATABASE_URL"] = "sqlite+aiosqlite:///./test.db"
-
-from httpx import AsyncClient, ASGITransport
-from asgi_lifespan import LifespanManager
+from unittest.mock import patch, AsyncMock
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlmodel import SQLModel
-
+from httpx import AsyncClient, ASGITransport
+from asgi_lifespan import LifespanManager
 from app.main import app
 from app.core.database import get_session
 
-
+os.environ["DATABASE_URL"] = "sqlite+aiosqlite:///./test.db"
 TEST_DATABASE_URL = os.environ["DATABASE_URL"]
+
+#creating test_engine for testing
 test_engine = create_async_engine(TEST_DATABASE_URL, echo=False)
+
+#testing async session
 TestAsyncSessionLocal: async_sessionmaker[AsyncSession] = async_sessionmaker(
     test_engine, expire_on_commit=False
 )
 
 
+#test initialize db
 @pytest_asyncio.fixture(scope="session")
 async def initialized_db() -> None: # type: ignore
     async with test_engine.begin() as conn:
@@ -31,6 +31,7 @@ async def initialized_db() -> None: # type: ignore
         await conn.run_sync(SQLModel.metadata.drop_all)
 
 
+#override get_session dependency for test
 async def override_get_session() -> AsyncSession: # type: ignore
     async with TestAsyncSessionLocal() as session:
         try:
@@ -39,15 +40,19 @@ async def override_get_session() -> AsyncSession: # type: ignore
             await session.close()
 
 
+#create test_app with overrided dependency 
 @pytest_asyncio.fixture(scope="session")
 async def test_app(initialized_db):
     app.dependency_overrides[get_session] = override_get_session
-    async with LifespanManager(app):
-        yield app
+    yield app
 
 
+
+#async client for handling request and Lifespan Manager
 @pytest_asyncio.fixture()
 async def client(test_app):
-    transport = ASGITransport(app=test_app)
-    async with AsyncClient(transport=transport, base_url="http://test") as ac:
-        yield ac
+    os.environ["TESTING"] = "1" 
+    async with LifespanManager(test_app):
+        transport = ASGITransport(app=test_app)
+        async with AsyncClient(transport=transport, base_url="http://test") as ac:
+            yield ac
