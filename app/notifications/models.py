03ca2e40c66bf51d0a830d5cf84c64dacaac0cce
Sanjiv Thapa <thapasvt12@gmail.com>
2025-08-16 14:00:50 +0545

    ✅ Updated index in model

--- app/notifications/models.py
+++ app/notifications/models.py
@@ -1,25 +1,26 @@
 from datetime import datetime, timezone
 from enum import Enum
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Column, Field, SQLModel, Text
 
 
 class NotificationType(str, Enum):
     GENERAL = ("general",)
     LIKE = ("like",)
+    NEW_BLOG = ("new_blog",)
     FOLLOW = ("follow",)
     COMMENT = "comment"
 
 
 class Notification(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    owner_id: int = Field(foreign_key="user.id")
-    blog_id: int | None = Field(default=None, foreign_key="blog.id")
-    triggered_by_user_id: int = Field(foreign_key="user.id")
+    owner_id: int = Field(foreign_key="user.id", index=True)
+    blog_id: int | None = Field(default=None, foreign_key="blog.id", index=True)
+    triggered_by_user_id: int = Field(foreign_key="user.id", index=True)
     notification_type: NotificationType = Field(
         default=NotificationType.GENERAL, index=True
     )
-    message: str = Field()
+    message: str = Field(sa_column=Column(Text))
     created_at: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc), index=True
     )
