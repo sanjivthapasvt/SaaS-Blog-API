5adc00d8b5b7358cdbc6b4ec2611c8f4570bc1fb
Sanjiv Thapa <thapasvt12@gmail.com>
2025-08-16 09:21:34 +0545

    ✅ Better responses and readable code in blogs/routes

--- app/blogs/routes.py
+++ app/blogs/routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, Query, UploadFile, Form
+from fastapi.responses import JSONResponse
 from fastapi.exceptions import HTTPException
 from fastapi_limiter.depends import RateLimiter
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -25,7 +26,10 @@ router = APIRouter()
 thumbnail_path: str = "blogs/thumbnail"
 
 
-@router.post("/blogs", dependencies=[Depends(RateLimiter(times=10, minutes=1, identifier=user_identifier))])
+@router.post(
+    "/blogs", 
+    dependencies=[Depends(RateLimiter(times=10, minutes=1, identifier=user_identifier))]
+)
 async def create_blog_route(
     title: str = Form(...),
     content: str = Form(...),
@@ -48,7 +52,10 @@ async def create_blog_route(
         )
 
         title = new_blog.title
-        return {"title": title, "detail": "Successfully created the blog"}
+        return JSONResponse(
+            content={"detail": f"Successfully created blog {title}"},
+            status_code=201,
+        )
 
     except HTTPException:
         raise
@@ -56,7 +63,10 @@ async def create_blog_route(
     except Exception as e:
         raise (HTTPException(status_code=500, detail=f"Something went wrong {str(e)}"))
 
-@router.post("/blogs/{blog_id}/like", dependencies=[Depends(RateLimiter(times=20, minutes=1, identifier=user_identifier))])
+@router.post(
+    "/blogs/{blog_id}/like", 
+    dependencies=[Depends(RateLimiter(times=20, minutes=1, identifier=user_identifier))]
+)
 async def like_blog_route(
     blog_id: int,
     current_user: CurrentUserRead = Depends(get_current_user),
@@ -71,7 +81,11 @@ async def like_blog_route(
         raise HTTPException(status_code=500, detail=f"Something went wrong while liking post {str(e)}")
 
 
-@router.get("/blogs", response_model=PaginatedResponse[BlogResponse], dependencies=[Depends(RateLimiter(times=60, minutes=1, identifier=user_identifier))])
+@router.get(
+    "/blogs",
+    response_model=PaginatedResponse[BlogResponse], 
+    dependencies=[Depends(RateLimiter(times=60, minutes=1, identifier=user_identifier))]
+)
 async def get_all_blogs_route(
     search: str = Query(default=None),
     limit :int = Query(10, ge=1),
@@ -80,15 +94,42 @@ async def get_all_blogs_route(
 ):
     """Retrieve all blogs with optional search and pagination."""
     try:
-        blogs = await get_all_blogs(session=session, search=search, limit=limit, offset=offset)
-        return blogs
+        blogs_result, total_result = await get_all_blogs(
+            session=session,
+            search=search, 
+            limit=limit, 
+            offset=offset
+        )
+        #validates response and set tags as list of strings
+        data = [
+            BlogResponse.model_validate(
+                blog.model_copy(update={
+                    "tags": [tag.title for tag in blog.tags]
+                })
+            )
+            for blog in blogs_result
+        ]
+
+        return PaginatedResponse[BlogResponse](
+            total=total_result,
+            limit=limit,
+            offset=offset,
+            data=data
+        )
+    
+    
     except HTTPException:
         raise
+    
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Something went wrong while getting blogs {str(e)}")
 
 
-@router.get("/blogs/liked", response_model=PaginatedResponse[BlogResponse], dependencies=[Depends(RateLimiter(times=30, minutes=1, identifier=user_identifier))])
+@router.get(
+    "/blogs/liked", 
+    response_model=PaginatedResponse[BlogResponse], 
+    dependencies=[Depends(RateLimiter(times=30, minutes=1, identifier=user_identifier))]
+)
 async def get_liked_blog_route(
     search: str = Query(default=None),
     limit :int = Query(10, ge=1),
@@ -98,29 +139,60 @@ async def get_liked_blog_route(
 ):
     """Retrieve blogs liked by the current user."""
     try:
-        return await get_liked_blogs(session=session, search=search, limit=limit, offset=offset, user_id=current_user.id) # type: ignore
+        blogs_result, total_result = await get_liked_blogs(
+            session=session, 
+            search=search, 
+            limit=limit, 
+            offset=offset, 
+            user_id=current_user.id
+        )
 
+        data = [
+            BlogResponse.model_validate(
+                blog.model_copy(update={
+                    "tags": [tag.title for tag in blog.tags]
+                })
+            )
+            for blog in blogs_result
+        ]
+
+        return PaginatedResponse[BlogResponse](
+            total=total_result,
+            limit=limit,
+            offset=offset,
+            data=data
+        )
+    
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"{str(e)}")
 
 
-@router.get("/blogs/{blog_id}", response_model=BlogContentResponse, dependencies=[Depends(RateLimiter(times=20, minutes=1, identifier=user_identifier))])
+@router.get(
+    "/blogs/{blog_id}", 
+    response_model=BlogContentResponse, 
+    dependencies=[Depends(RateLimiter(times=20, minutes=1, identifier=user_identifier))]
+)
 async def get_specefic_blog_route(
     blog_id: int, 
     session: AsyncSession = Depends(get_session)
 ):
     """Retrieve a specific blog post by ID."""
     try:
-        return await get_blog_by_id(session=session, blog_id=blog_id)
-
+        blog = await get_blog_by_id(session=session, blog_id=blog_id)
+        return blog
+    
     except HTTPException:
         raise
+    
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"{str(e)}")
 
 
 
-@router.patch("/blogs/{blogs_id}", dependencies=[Depends(RateLimiter(times=10, minutes=1, identifier=user_identifier))])
+@router.patch(
+    "/blogs/{blogs_id}", 
+    dependencies=[Depends(RateLimiter(times=10, minutes=1, identifier=user_identifier))]
+)
 async def update_blog_route(
     blog_id: int,
     title: str | None = Form(None),
@@ -131,8 +203,20 @@ async def update_blog_route(
 ):
     """Update a blog post's title, content, or thumbnail."""
     try:
-        return await update_blog(session=session, blog_id=blog_id, title=title, content=content, thumbnail=thumbnail, current_user=current_user.id, thumbnail_path=thumbnail_path)
-        
+        updated_blog =  await update_blog(
+            session=session, 
+            blog_id=blog_id, 
+            title=title, 
+            content=content, 
+            thumbnail=thumbnail, 
+            current_user=current_user.id, 
+            thumbnail_path=thumbnail_path
+        )
+
+        return JSONResponse(
+            content={"detail": f"Successfully updated blog {title}"},
+            status_code=200,
+        )
     except HTTPException:
         raise
 
@@ -140,7 +224,10 @@ async def update_blog_route(
         raise HTTPException(status_code=500, detail=f"Something went wrong{str(e)}")
 
 
-@router.delete("/blogs/{blog_id}", dependencies=[Depends(RateLimiter(times=10, minutes=1, identifier=user_identifier))])
+@router.delete(
+    "/blogs/{blog_id}", 
+    dependencies=[Depends(RateLimiter(times=10, minutes=1, identifier=user_identifier))]
+)
 async def delete_blog_route(
     blog_id:int,
     session: AsyncSession =  Depends(get_session), 
@@ -149,8 +236,10 @@ async def delete_blog_route(
     """Delete a blog post by ID."""
     try:
         title = await delete_blog(session=session, blog_id=blog_id, current_user=current_user.id)
-        return {"title": title, "detail": "Successfully deleted blog"}
-    
+        return JSONResponse(
+            content={"detail": f"Successfully deleted blog {title}"},
+            status_code=200,
+        )
     except HTTPException:
         raise
 
