dcc4fb19e4767857254b8ee470a00195693872ec
Sanjiv Thapa <thapasvt12@gmail.com>
2025-08-16 13:55:59 +0545

    ✅ Formatted code using black and isort

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,47 +11,46 @@ from app.core.database import get_session
 os.environ["DATABASE_URL"] = "sqlite+aiosqlite:///./test.db"
 TEST_DATABASE_URL = os.environ["DATABASE_URL"]
 
-#creating test_engine for testing
+# creating test_engine for testing
 test_engine = create_async_engine(TEST_DATABASE_URL, echo=False)
 
-#testing async session
+# testing async session
 TestAsyncSessionLocal: async_sessionmaker[AsyncSession] = async_sessionmaker(
     test_engine, expire_on_commit=False
 )
 
 
-#test initialize db
+# test initialize db
 @pytest_asyncio.fixture(scope="session")
-async def initialized_db() -> None: # type: ignore
+async def initialized_db() -> None:  # type: ignore
     async with test_engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
-    yield # type: ignore
+    yield  # type: ignore
     # drop tables after tests
     async with test_engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.drop_all)
 
 
-#override get_session dependency for test
-async def override_get_session() -> AsyncSession: # type: ignore
+# override get_session dependency for test
+async def override_get_session() -> AsyncSession:  # type: ignore
     async with TestAsyncSessionLocal() as session:
         try:
-            yield session # type: ignore
+            yield session  # type: ignore
         finally:
             await session.close()
 
 
-#create test_app with overrided dependency 
+# create test_app with overrided dependency
 @pytest_asyncio.fixture(scope="session")
 async def test_app(initialized_db):
     app.dependency_overrides[get_session] = override_get_session
     yield app
 
 
-
-#async client for handling request and Lifespan Manager
+# async client for handling request and Lifespan Manager
 @pytest_asyncio.fixture()
 async def client(test_app):
-    os.environ["TESTING"] = "1" 
+    os.environ["TESTING"] = "1"
     async with LifespanManager(test_app):
         transport = ASGITransport(app=test_app)
         async with AsyncClient(transport=transport, base_url="http://test") as ac:
