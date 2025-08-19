76179fed414be4b6f88349c5f4493defae47dd58
Sanjiv Thapa <thapasvt12@gmail.com>
2025-08-19 14:13:01 +0545

    ✅ Used Optional instead of int | None

--- app/notifications/models.py
+++ app/notifications/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
 from enum import Enum
+from typing import Optional
 
 from sqlmodel import Column, Field, SQLModel, Text
 
@@ -13,9 +14,9 @@ class NotificationType(str, Enum):
 
 
 class Notification(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     owner_id: int = Field(foreign_key="user.id", index=True)
-    blog_id: int | None = Field(default=None, foreign_key="blog.id", index=True)
+    blog_id: Optional[int] = Field(default=None, foreign_key="blog.id", index=True)
     triggered_by_user_id: int = Field(foreign_key="user.id", index=True)
     notification_type: NotificationType = Field(
         default=NotificationType.GENERAL, index=True
