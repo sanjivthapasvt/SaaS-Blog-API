ce8020735350a154f4e16aa4d532a72bdc0cdd56
Sanjiv Thapa <thapasvt12@gmail.com>
2025-08-19 14:14:15 +0545

    ✅ Used Optional instead of int | None

--- app/models/blog_like_link.py
+++ app/models/blog_like_link.py
@@ -1,6 +1,6 @@
 from sqlmodel import Field, SQLModel
-
+from typing import Optional
 
 class BlogLikeLink(SQLModel, table=True):
-    blog_id: int | None = Field(default=None, foreign_key="blog.id", primary_key=True)
-    user_id: int | None = Field(default=None, foreign_key="user.id", primary_key=True)
+    blog_id: Optional[int] = Field(default=None, foreign_key="blog.id", primary_key=True)
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id", primary_key=True)
