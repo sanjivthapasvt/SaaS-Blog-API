c5643532fee57b5fdd05144a411ebaf5d6114e5d
Sanjiv Thapa <thapasvt12@gmail.com>
2025-08-22 18:12:09 +0545

    ✅ New Migrations uuid on user and slug on blog

--- app/alembic/versions/91aaf195bfc4_notificatoin.py
+++ app/alembic/versions/91aaf195bfc4_notificatoin.py
@@ -9,6 +9,7 @@ Create Date: 2025-08-16 07:46:37.046716
 from typing import Sequence, Union
 
 import sqlalchemy as sa
+
 from alembic import op
 
 # revision identifiers, used by Alembic.
--- app/alembic/versions/daec8cc671ad_updated_index_on_notificatoin.py
+++ app/alembic/versions/daec8cc671ad_updated_index_on_notificatoin.py
@@ -9,6 +9,7 @@ Create Date: 2025-08-16 13:58:21.941936
 from typing import Sequence, Union
 
 import sqlalchemy as sa
+
 from alembic import op
 
 # revision identifiers, used by Alembic.
