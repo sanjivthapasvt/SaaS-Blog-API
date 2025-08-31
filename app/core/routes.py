edacf7d43fa9405471aa0f00dd1c4e800c273f76
Sanjiv Thapa <thapasvt12@gmail.com>
2025-08-31 17:55:58 +0545

    🧹 Updated router to match new routers

--- app/core/routes.py
+++ app/core/routes.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 
-from app.auth.google_routes import router as google_auth_router
-from app.auth.routes import router as auth_router
-from app.blogs.comment_routes import router as comment_router
-from app.blogs.routes import router as blog_router
+from app.auth.router import router as auth_router
+from app.blogs.router import router as blog_router
 from app.notifications.routes import router as notification_router
 from app.realtime.routes import router as realtime_router
 from app.users.router import router as users_router
@@ -18,10 +16,8 @@ def setup_routes(app: FastAPI) -> None:
         return {"ping": "pong"}
 
     # Include API routers
-    app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
-    app.include_router(google_auth_router, prefix="/api/auth", tags=["Auth"])
-    app.include_router(blog_router, prefix="/api", tags=["Blog"])
-    app.include_router(comment_router, prefix="/api", tags=["Comments"])
+    app.include_router(auth_router, prefix="/api/auth")
+    app.include_router(blog_router, prefix="/api")
     app.include_router(notification_router, prefix="/api", tags=["Notification"])
     app.include_router(realtime_router, prefix="/api", tags=["Realtime"])
     app.include_router(users_router, prefix="/api")
